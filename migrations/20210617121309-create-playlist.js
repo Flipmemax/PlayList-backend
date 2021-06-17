b3c16f5c4fe14301f8437b64dfbc85971d7e8667
Flipmemax <maxphoogland@gmail.com>
2021-06-17 15:11:13 +0200

    setup playlist model/migration -- relations included

--- migrations/20210617121309-create-playlist.js
+++ migrations/20210617121309-create-playlist.js
@@ -1,27 +1,38 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('playlists', {
+    await queryInterface.createTable("playlists", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('playlists');
-  }
+    await queryInterface.dropTable("playlists");
+  },
 };
